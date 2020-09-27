1fc90fb3f676eb2582ef5831fa7148964acc60a3
Caitlin O'Callaghan <caitlinocallaghan@Caitlins-MBP.lan>
2020-09-27 16:55:42 -0500

    adding search bar, image, navbar to general cities page

--- burninup-app/src/views/Cities.tsx
+++ burninup-app/src/views/Cities.tsx
@@ -3,16 +3,38 @@ import "./App.css";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import Image from "react-bootstrap/Image";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import FormControl from "react-bootstrap/FormControl";
 
 function Cities() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Burnin' Up</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link href="#link">Countries</Nav.Link>
+              <Nav.Link href="#link">Cities</Nav.Link>
+              <Nav.Link href="#link">Global Climate Change</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <h1>Cities page</h1>
-        <div>
-          <Button variant="primary">Landing page</Button>{" "}
-        </div>
         <Image src={require("../assets/map.jpg")} fluid />
+        <Form>
+          <Form.Group>
+            <Form.Label>Search Bar</Form.Label>
+            <Form.Control placeholder="Enter search" />
+          </Form.Group>
+        </Form>
+        <Form></Form>
         <Table striped bordered hover size="sm" variant="dark">
           <thead>
             <tr>
