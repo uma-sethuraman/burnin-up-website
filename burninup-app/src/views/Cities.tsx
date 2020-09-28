a9c6deaf22ad961cebcdd8c31c342614fcae9f8b
LaurenMangibin <lauren.mangibin@gmail.com>
2020-09-28 11:35:10 -0500

    Added links to cities and countries in cities general page

--- burninup-app/src/views/Cities.tsx
+++ burninup-app/src/views/Cities.tsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import React from "react";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import Image from "react-bootstrap/Image";
 import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import FormControl from "react-bootstrap/FormControl";
-import Navbar from './components/OurNavbar';
+import Navbar from "./components/OurNavbar";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
 
 function Cities() {
   return (
@@ -18,8 +21,40 @@ function Cities() {
         <Image src={require("../assets/map.jpg")} fluid />
         <Form>
           <Form.Group>
-            <Form.Label>Search Bar</Form.Label>
-            <Form.Control placeholder="Enter search" />
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+            <>
+              {[
+                "Primary",
+                "Secondary",
+                "Success",
+                "Info",
+                "Warning",
+                "Danger",
+              ].map((variant) => (
+                <DropdownButton
+                  as={ButtonGroup}
+                  key={variant}
+                  id={`dropdown-variants-${variant}`}
+                  variant={variant.toLowerCase()}
+                  title={variant}
+                >
+                  <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                  <Dropdown.Item eventKey="3" active>
+                    Active Item
+                  </Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                </DropdownButton>
+              ))}
+            </>
           </Form.Group>
         </Form>
         <Form></Form>
@@ -31,20 +66,21 @@ function Cities() {
               <th>Air Quality</th>
             </tr>
           </thead>
+
           <tbody>
             <tr>
-              <td>Austin</td>
-              <td>United States</td>
+              <td><Link to="/cities/austin">Austin</Link></td>
+              <td><Link to="/countries/USA">United States</Link></td>
               <td>2</td>
             </tr>
             <tr>
-              <td>Beijing</td>
-              <td>China</td>
+              <td><Link to="/cities/beijing">Beijing</Link></td>
+              <td><Link to="/countries/china">China</Link></td>
               <td>3</td>
             </tr>
             <tr>
-              <td>Paris</td>
-              <td>France</td>
+              <td><Link to="/cities/paris">Paris</Link></td>
+              <td><Link to="/countries/france">France</Link></td>
               <td>4</td>
             </tr>
           </tbody>
