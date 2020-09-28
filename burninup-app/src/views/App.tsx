130fd75ff5187a9c107c71003425cf23ba511085
samantha3pen <samtuapen@utexas.edu>
2020-09-27 19:10:23 -0500

    basic landing page made. will make pretty later

--- burninup-app/src/views/App.tsx
+++ burninup-app/src/views/App.tsx
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Navbar from './components/OurNavbar';
+import Image from "react-bootstrap/Image"; 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <h1>
-          "I'm burning up, burning up for you baby." - Jonas Brothers (2008)
-        </h1>
-        <Button variant="primary" href = "/about">About Us</Button>{' '}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Burnin Up </h1>
+        <p>As the Jonas Brothers would say, the world is "burning up, burning up for you baby." </p>
+        <h2>(this meme is how some of y'all be acting, but the world is in fact NOT fine)</h2>
+        <Image src={require("../assets/fire-meme.jpg")} fluid />
       </header>
-      
     </div>
   );
 }
