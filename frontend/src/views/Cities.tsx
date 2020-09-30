66c07ecdae99ccd17deca2ef8f73ead37a4a747e
LaurenMangibin <lauren.mangibin@gmail.com>
2020-09-30 12:50:58 -0500

    Added filter buttons

--- frontend/src/views/Cities.tsx
+++ frontend/src/views/Cities.tsx
@@ -21,7 +21,7 @@ function Cities() {
         <Image src={require("../assets/city-landing-photo-singapore.jpg")} fluid />
         <Form>
           <Form.Group>
-            <Form inline>
+            <Form>
               <FormControl
                 type="text"
                 placeholder="Search"
@@ -30,30 +30,42 @@ function Cities() {
               <Button variant="outline-info">Search</Button>
             </Form>
             <>
-              {[
-                "Primary",
-                "Secondary",
-                "Success",
-                "Info",
-                "Warning",
-                "Danger",
-              ].map((variant) => (
-                <DropdownButton
-                  as={ButtonGroup}
-                  key={variant}
-                  id={`dropdown-variants-${variant}`}
-                  variant={variant.toLowerCase()}
-                  title={variant}
-                >
-                  <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                  <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                  <Dropdown.Item eventKey="3" active>
-                    Active Item
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-                </DropdownButton>
-              ))}
+            <ButtonGroup>
+            <DropdownButton
+              title= {"Zip Code"} >
+                  <Dropdown.Item eventKey="1">78739</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">20134 </Dropdown.Item>
+                  <Dropdown.Item eventKey="3"> 410 </Dropdown.Item>
+
+            </DropdownButton>
+            <DropdownButton
+              title= {"Elevation"} >
+                  <Dropdown.Item eventKey="1">Less than 1000</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Less than 5000 </Dropdown.Item>
+                  <Dropdown.Item eventKey="3">Less than 10,000</Dropdown.Item>
+            </DropdownButton>
+
+            <DropdownButton
+              title= {"Time Zone"} >
+                  <Dropdown.Item eventKey="1">China Standard Time</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Central Standard Time </Dropdown.Item>
+                  <Dropdown.Item eventKey="3">Central European Summer Time </Dropdown.Item>
+            </DropdownButton>
+
+            <DropdownButton
+              title= {"Location"} >
+                  <Dropdown.Item eventKey="1">Asia</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Europe </Dropdown.Item>
+                  <Dropdown.Item eventKey="3">North America</Dropdown.Item>
+            </DropdownButton>
+            
+            <DropdownButton
+              title= {"Population"} >
+                  <Dropdown.Item eventKey="1">Less than 1000</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">Less than 1 million </Dropdown.Item>
+                  <Dropdown.Item eventKey="3">Less than 5 million</Dropdown.Item>
+            </DropdownButton>
+            </ButtonGroup>
             </>
           </Form.Group>
         </Form>
