16dc84bdcd63c4cce8bfea661aa1327f7caf2e62
samantha3pen <samtuapen@utexas.edu>
2020-09-29 23:45:52 -0500

    updated sam's pages with data from api

--- frontend/src/views/Paris.tsx
+++ frontend/src/views/Paris.tsx
@@ -11,6 +11,8 @@ function Paris() {
             <header className="App-header">
                 <h1>Paris, France</h1> 
                 <br />
+                <Image src={require("../assets/paris-skyline.jpg")} fluid />
+                <br />
                 <Table striped bordered hover size="sm" variant="dark">
                     <tbody>
                         <tr>
@@ -25,9 +27,60 @@ function Paris() {
                         <td>Elevation: </td>
                         <td>115ft</td>
                         </tr>
+                        <tr>
+                        <td>Latitude: </td>
+                        <td>48.8566</td>
+                        </tr>
+                        <tr>
+                        <td>Longitude: </td>
+                        <td>2.3522</td>
+                        </tr>
+                        <tr>
+                        <td>Fire Hazard Index: </td>
+                        <td>0.875</td>
+                        </tr>
+                        <tr>
+                        <td>Air Quality Index per US EPA Standard: </td>
+                        <td>57</td>
+                        </tr>
+                        <tr>
+                        <td>Particulate Matter: </td>
+                        <td>20.375 μg/m3</td>
+                        </tr>
+                        <tr>
+                        <td>Ozone: </td>
+                        <td>0.9375 ppb</td>
+                        </tr>
+                        <tr>
+                        <td>Nitrogen Dioxide: </td>
+                        <td>32.5 ppb</td>
+                        </tr>
+                        <tr>
+                        <td>Carbon Monoxide: </td>
+                        <td>0.4375 ppm</td>
+                        </tr>
+                        <tr>
+                        <td>Sulfur Dioxide: </td>
+                        <td>0.25 ppb</td>
+                        </tr>
+                        <tr>
+                        <td>Health Concern Level Based On EPA Standard: </td>
+                        <td>Moderate</td>
+                        </tr>
+                        <tr>
+                        <td>ClimaCell Pollen Index for Trees: </td>
+                        <td>0 Climacell Pollen Index</td>
+                        </tr>
+                        <tr>
+                        <td>ClimaCell Pollen Index for Weeds: </td>
+                        <td>0 Climacell Pollen Index</td>
+                        </tr>
+                        <tr>
+                        <td>ClimaCell Pollen Index for Grass: </td>
+                        <td>0 Climacell Pollen Index</td>
+                        </tr>
                     </tbody>
                     </Table>
-                <Image src={require("../assets/paris-skyline.jpg")} fluid />
                 <br />
                 <Image src={require("../assets/paris-location.jpg")} fluid />
                 <br />
