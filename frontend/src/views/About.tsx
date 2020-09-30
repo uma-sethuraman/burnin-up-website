364193f5022f09a49b65761d17d1a1dd67cec644
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-09-30 02:47:31 -0500

    got gitlab issues working dynamically in about page

--- frontend/src/views/About.tsx
+++ frontend/src/views/About.tsx
@@ -6,65 +6,63 @@ import axios from "axios";
 import { useState } from "react";
 
 function About() {
-  const [stat, changeStat] = useState<Array<number>>([-1, -1, -1, -1, -1, -1]);
 
-  function apiRequest(url: string, index: number) {
-    axios
-      .get(url)
-      .catch(function (error) {
-        console.log(error.toJSON());
-      })
-      .then((r) => {
-        const stats: Gitlab = JSON.parse(JSON.stringify(r)) as Gitlab;
-        let tempArray = stat;
-        tempArray[index] = stats.data.statistics.counts.all;
-        changeStat(tempArray);
-        console.log(stat);
-        console.log(tempArray);
-      });
+  /* Statistics for GitLab Issues */
+  const [statsCL, changeStatsCL] = useState(-1);
+  const [statsCO, changeStatsCO] = useState(-1);
+  const [statsCS, changeStatsCS] = useState(-1);
+  const [statsLM, changeStatsLM] = useState(-1);
+  const [statsST, changeStatsST] = useState(-1);
+  const [statsUS, changeStatsUS] = useState(-1);
+
+  /* Retrieves data about GitLab issues per contributor */
+  function issuesApiRequest() {
+
+    /* Requests for all contributors */
+    const reqCL = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinlien");
+    const reqCO = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinocallaghan");
+    const reqCS = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=cherrysun9");
+    const reqLM = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=lauren.mangibin");
+    const reqST = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=samantha3pen");
+    const reqUS = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=uma-sethuraman");
+    
+    /* Change statistics variables for all contributors */
+    axios.all([reqCL, reqCO, reqCS, reqLM, reqST, reqUS]).then(axios.spread((...responses) => {
+      const responseCL: Gitlab = JSON.parse(JSON.stringify(responses[0])) as Gitlab;
+      changeStatsCL(responseCL.data.statistics.counts.all);
+      const responseCO: Gitlab = JSON.parse(JSON.stringify(responses[1])) as Gitlab;
+      changeStatsCO(responseCO.data.statistics.counts.all);
+      const responseCS: Gitlab = JSON.parse(JSON.stringify(responses[2])) as Gitlab;
+      changeStatsCS(responseCS.data.statistics.counts.all);
+      const responseLM: Gitlab = JSON.parse(JSON.stringify(responses[3])) as Gitlab;
+      changeStatsLM(responseLM.data.statistics.counts.all);
+      const responseST: Gitlab = JSON.parse(JSON.stringify(responses[4])) as Gitlab;
+      changeStatsST(responseST.data.statistics.counts.all);
+      const responseUS: Gitlab = JSON.parse(JSON.stringify(responses[5])) as Gitlab;
+      changeStatsUS(responseUS.data.statistics.counts.all);
+    })).catch(errors => {
+      console.log(errors.toJSON());
+    })
   }
 
   return (
     <div className="App">
-      {apiRequest(
-        "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinlien",
-        0
-      )}
-      {apiRequest(
-        "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinocallaghan",
-        1
-      )}
-      {apiRequest(
-        "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=cherrysun9",
-        2
-      )}
-      {apiRequest(
-        "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=lauren.mangibin",
-        3
-      )}
-      {apiRequest(
-        "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=samantha3pen",
-        4
-      )}
-      {apiRequest(
-        "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=uma-sethuraman",
-        5
-      )}
+      {issuesApiRequest()}
       <Navbar />
       <header className="App-header">
         <h1> THIS IS THE ABOUT PAGE </h1>
         <h4>Caitlin Lien</h4>
-        <h1>{stat[0]}</h1>
+        <p>issues: {statsCL}</p>
         <h4>Caitlin O'Callaghan</h4>
-        <h1>{stat[1]}</h1>
+        <p>issues: {statsCO}</p>
         <h4>Cherry Sun</h4>
-        <h1>{stat[2]}</h1>
+        <p>issues: {statsCS}</p>
         <h4>Lauren Mangibin</h4>
-        <h1>{stat[3]}</h1>
+        <p>issues: {statsLM}</p>
         <h4>Samantha Tuapen</h4>
-        <h1>{stat[4]}</h1>
+        <p>issues: {statsST}</p>
         <h4>Uma Sethuraman</h4>
-        <h1>{stat[5]}</h1>
+        <p>issues: {statsUS}</p>
       </header>
     </div>
   );
