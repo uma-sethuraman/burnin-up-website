b2b5bdc7e606b0efd70b2593dc867d4bdf38578a
cherrysun9 <cherrysun9@utexas.edu>
2020-10-06 13:52:45 -0500

    main.py updated

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
 from sqlalchemy import Column, String, Integer
 from flask import request
 import urllib
 import os
 import json
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,23 +15,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.debug = True
 print("before config")
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://supremeleader:steven04@burninup-db-1.cgloqeyb6wie.us-east-2.rds.amazonaws.com/burninup-db-1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://supremeleader:steven04@burninup-db-1.cgloqeyb6wie.us-east-2.rds.amazonaws.com:5432/postgres'
 
-print("set it to false")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-print("before db")
-#db = SQLAlchemy(app)
-#ma = Marshmallow(app)
-print("before engine")
-eng = create_engine('postgresql://supremeleader:steven04@burninup-db-1.cgloqeyb6wie.us-east-2.rds.amazonaws.com:5432/postgres')
-print("after engine")
-inspector = inspect(eng)
-# Get table information
-print(inspector.get_table_names())
-print("after showing tables")
 
-print("here")
-""""
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
 
 class Country(db.Model):
     country_id = db.Column(db.Integer, primary_key=True)
@@ -41,7 +34,6 @@ class Country(db.Model):
         self.country_region = country_region
 
 db.create_all()
-
 
 class CountrySchema(ma.Schema):
     class Meta:
@@ -61,26 +53,11 @@ for item in data[1]:
 db.session.add_all(country_list)
 db.session.commit()
 
-
-        country_schema.jsonify(new_country)
-@app.route('/country', methods=['POST'])
-def append_country():
-    country_name = request.json['country']
-    air_quality = request.json['air_quality']
-    income = request.json['income']
-    population = request.json['population']
-
-    new_country = Country(country_name,air_quality,income,population)
-    db.session.add(new_country)
-    db.session.commit()
-    return country_schema.jsonify(new_country)
-
-
 @app.route('/country', methods=['GET'])
 def get_country():
     all_country = Country.query.all()
     result = countries_schema.dump(all_country)
     return jsonify(result)
-"""""''
+
 if __name__ == '__main__':
     app.run(debug=True)
