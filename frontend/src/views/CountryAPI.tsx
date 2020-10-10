1864dee3ef88ce88ae72ec01b13eca4f6201ee14
cherrysun9 <cherrysun9@utexas.edu>
2020-10-09 21:06:14 -0500

    added countryapi

---
+++ frontend/src/views/CountryAPI.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function CountryAPI() {
+
+  const [currentTest, setCurrentTest] = useState("hi");
+  useEffect(() => {
+      fetch('/api/country').then(res => res.json().then(data => {
+        setCurrentTest(data.test)
+      })
+    );
+  }, []);
+
+  return (
+     <div className="App">
+      <p> {currentTest} </p>
+    </div>
+  );
+
+}
+export default CountryAPI;
