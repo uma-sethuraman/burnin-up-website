3772bd5e6a30f23f6722182d4f157a30c790c2aa
lauren.mangibin <lauren.mangibin@gmail.com>
2020-10-12 18:53:11 -0500

    Changed routes to exact component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -29,29 +29,35 @@ ReactDOM.render(
   <Router>
       <Route path="/about" exact component = {About}/>
       <Route path="/" exact component = {App}/>
+      <Route path="/cities" exact component = {Cities} />
+      <Route path="/countries" exact component = {Countries} />
+
+ 
+      {/* <Route path="/landing" exact component = {App}/> */}
+      {/* <Route path="/countries/USA" exact component = {USA}/>
+      <Route path="/countries/China" exact component = {China} />
+      <Route path="/countries/France" exact component = {France} />
+      <Route path="/cities/Beijing" exact component = {Beijing />} />
+      <Route path="/cities/Paris" exact component = {Paris />} /> */}
+      <Route path="/cities/Austin" exact component = {Austin} />
       
+      <Route path="/city" exact component = {CityInstance } />
+      {/* <Route path="/climatechange/2013" exact component = {Year2013 />} />
+      <Route path="/climatechange/2014" exact component = {Year2014 />} />
+      <Route path="/climatechange/2015" exact component = {Year2015 />} /> */}
+      <Route path="/climatechange" exact component = {GeneralYears} />
+      <Route path="/year" exact component = {YearInstance} />
+    
+      <Route path="/CountryAPI" exact component = {CountryAPI} />
 
-
-      {/* { <Route path="/landing" render={(props) => <App />} /> }
-      <Route path="/countries/USA" render={(props) => <USA />} />
-      <Route path="/countries/China" render={(props) => <China />} />
-      <Route path="/countries/France" render={(props) => <France />} />
-      <Route path="/cities/Beijing" render={(props) => <Beijing />} />
-      <Route path="/cities/Paris" render={(props) => <Paris />} />
-      <Route path="/cities/Austin" render={(props) => <Austin />} />
-      <Route path="/cities" render={(props) => <Cities />} />
-      <Route path="/city" render={(props) => <CityInstance />} />
-      <Route path="/climatechange/2013" render={(props) => <Year2013 />} />
-      <Route path="/climatechange/2014" render={(props) => <Year2014 />} />
-      <Route path="/climatechange/2015" render={(props) => <Year2015 />} />
-      <Route path="/climatechange" render={(props) => <GeneralYears />} />
-      <Route path="/year" render={(props) => <YearInstance />} />
-      <Route path="/countries" render={(props) => <Countries />} />
-      <Route path="/CountryAPI" render={(props) => <CountryAPI />} /> */} 
-
-      {/* <Redirect to="/landing" /> */}
+      {/* //add protections later? when removed, all the pages work.. else, all pages
+      //redirect to landing page */}
+      {/* <Redirect to="/" /> */}
+      
   </Router>,
   document.getElementById("root")
+
+  
 );
 
 // If you want your app to work offline and load faster, you can change
