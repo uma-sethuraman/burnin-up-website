ad881bd74f017aa0f465f1acbb8e02713c83f7fd
Caitlin O'Callaghan <caitlinocallaghan@Caitlins-MacBook-Pro.local>
2020-10-13 13:01:30 -0500

    fixing about page

--- frontend/src/views/About.tsx
+++ frontend/src/views/About.tsx
@@ -4,9 +4,63 @@ import Navbar from "./components/OurNavbar";
 import axios from "axios";
 import { useState } from "react";
 import Image from "react-bootstrap/Image";
-import CountryAPI from './CountryAPI'
+import CountryAPI from "./CountryAPI";
+
+interface GroupMember {
+  name: string;
+  email: string;
+  username?: string;
+  commits: number;
+  issues: number;
+  unittests: number;
+}
 
 function About() {
+  const [members, changeMembers] = useState<GroupMember[]>([
+    {
+      name: "Caitlin Lien",
+      email: "caitlinlien@utexas.edu",
+      username: "caitlinlien",
+      commits: 0,
+      issues: 0,
+      unittest: 0,
+    },
+    {
+      name: "Caitlin O'Callaghan",
+      email: "caitlinocallaghan@Caitlins-MBP.lan",
+      commits: 0,
+      issues: 0,
+      unittest: 0,
+    },
+    {
+      name: "Cherry Sun",
+      email: "cherrysun9@utexas.edu",
+      commits: 0,
+      issues: 0,
+      unittest: 0,
+    },
+    {
+      name: "Caitlin O'Callaghan",
+      email: "lauren.mangibin@gmail.com",
+      commits: 0,
+      issues: 0,
+      unittest: 0,
+    },
+    {
+      name: "Caitlin O'Callaghan",
+      email: "caitlinocallaghan@Caitlins-MBP.lan",
+      commits: 0,
+      issues: 0,
+      unittest: 0,
+    },
+    {
+      name: "Caitlin O'Callaghan",
+      email: "caitlinocallaghan@Caitlins-MBP.lan",
+      commits: 0,
+      issues: 0,
+      unittest: 0,
+    },
+  ]);
 
   /* Commits per contributor from GitLab API */
   const [commitsCL, changeCommitsCL] = useState(-1);
@@ -26,32 +80,80 @@ function About() {
 
   /* Retrieves data about GitLab issues per contributor */
   function issuesApiRequest() {
-
-    /* Requests for all contributors */
-    const reqCL = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinlien");
-    const reqCO = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinocallaghan");
-    const reqCS = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=cherrysun9");
-    const reqLM = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=lauren.mangibin");
-    const reqST = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=samantha3pen");
-    const reqUS = axios.get("https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=uma-sethuraman");
-    
-    /* Change statistics variables for all contributors */
-    axios.all([reqCL, reqCO, reqCS, reqLM, reqST, reqUS]).then(axios.spread((...responses) => {
-      const responseCL: Gitlab = JSON.parse(JSON.stringify(responses[0])) as Gitlab;
-      changeStatsCL(responseCL.data.statistics.counts.all);
-      const responseCO: Gitlab = JSON.parse(JSON.stringify(responses[1])) as Gitlab;
-      changeStatsCO(responseCO.data.statistics.counts.all);
-      const responseCS: Gitlab = JSON.parse(JSON.stringify(responses[2])) as Gitlab;
-      changeStatsCS(responseCS.data.statistics.counts.all);
-      const responseLM: Gitlab = JSON.parse(JSON.stringify(responses[3])) as Gitlab;
-      changeStatsLM(responseLM.data.statistics.counts.all);
-      const responseST: Gitlab = JSON.parse(JSON.stringify(responses[4])) as Gitlab;
-      changeStatsST(responseST.data.statistics.counts.all);
-      const responseUS: Gitlab = JSON.parse(JSON.stringify(responses[5])) as Gitlab;
-      changeStatsUS(responseUS.data.statistics.counts.all);
-    })).catch(errors => {
+    const larry: string =
+      "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=";
+    const requestArray = [];
+    for (const member of members) {
+      requestArray.push(larry + member.username);
+    }
+    axios.all([...requestArray]).then(
+      axios.spread((...responses) => {
+        const membersCopy: GroupMember[] = JSON.parse(JSON.stringify(members));
+        for (let i = 0; i < responses.length; i++) {
+          const response: Gitlab = JSON.parse(
+            JSON.stringify(responses[i])
+          ) as Gitlab;
+          membersCopy[i].issues = response.data.statistics.counts.all;
+        }
+        changeMembers(membersCopy);
+      })
+    ).catch((errors) => {
       console.log(errors.toJSON());
-    })
+    });
+    /* Requests for all contributors */
+    // const reqCL = axios.get(
+    //   "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinlien"
+    // );
+    // const reqCO = axios.get(
+    //   "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=caitlinocallaghan"
+    // );
+    // const reqCS = axios.get(
+    //   "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=cherrysun9"
+    // );
+    // const reqLM = axios.get(
+    //   "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=lauren.mangibin"
+    // );
+    // const reqST = axios.get(
+    //   "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=samantha3pen"
+    // );
+    // const reqUS = axios.get(
+    //   "https://gitlab.com/api/v4/projects/21349576/issues_statistics?author_username=uma-sethuraman"
+    // );
+
+    /* Change statistics variables for all contributors */
+    // axios
+    //   .all([reqCL, reqCO, reqCS, reqLM, reqST, reqUS])
+    //   .then(
+    //     axios.spread((...responses) => {
+    //       const responseCL: Gitlab = JSON.parse(
+    //         JSON.stringify(responses[0])
+    //       ) as Gitlab;
+    //       changeStatsCL(responseCL.data.statistics.counts.all);
+    //       const responseCO: Gitlab = JSON.parse(
+    //         JSON.stringify(responses[1])
+    //       ) as Gitlab;
+    //       changeStatsCO(responseCO.data.statistics.counts.all);
+    //       const responseCS: Gitlab = JSON.parse(
+    //         JSON.stringify(responses[2])
+    //       ) as Gitlab;
+    //       changeStatsCS(responseCS.data.statistics.counts.all);
+    //       const responseLM: Gitlab = JSON.parse(
+    //         JSON.stringify(responses[3])
+    //       ) as Gitlab;
+    //       changeStatsLM(responseLM.data.statistics.counts.all);
+    //       const responseST: Gitlab = JSON.parse(
+    //         JSON.stringify(responses[4])
+    //       ) as Gitlab;
+    //       changeStatsST(responseST.data.statistics.counts.all);
+    //       const responseUS: Gitlab = JSON.parse(
+    //         JSON.stringify(responses[5])
+    //       ) as Gitlab;
+    //       changeStatsUS(responseUS.data.statistics.counts.all);
+    //     })
+    //   )
+    //   .catch((errors) => {
+    //     console.log(errors.toJSON());
+    //   });
   }
 
   /* Retrieves number of commits per contributor from GitLab API */
@@ -82,7 +184,7 @@ function About() {
       })
       .catch(function (error) {
         console.log(error.toJSON());
-      })
+      });
   };
 
   return (
@@ -91,64 +193,108 @@ function About() {
       {issuesApiRequest()}
       <Navbar />
       <body className="About-body">
-        <div className = "h3_about">
+        <div className="h3_about">
           <h3>About Us</h3>
         </div>
         <div className="row">
           <div className="h2_about">
             <div className="column">
-              <Image src={require("../assets/CaitlinLien.JPG")} height="250" roundedCircle/>
-              <h2><b>Caitlin Lien</b></h2>
-              <p>Issues: {statsCL}</p>
+              <Image
+                src={require("../assets/CaitlinLien.JPG")}
+                height="250"
+                roundedCircle
+              />
+              <h2>
+                <b>Caitlin Lien</b>
+              </h2>
+              <p>Issues: {members[0].issues}</p>
               <p>Commits: {commitsCL}</p>
               <p>Unit Tests: 0</p>
             </div>
             <div className="column">
-              <Image src={require("../assets/caitlin-o-headshot.jpeg")} height="250" roundedCircle/>
-              <h2><b>Caitlin O'Callaghan</b></h2>
+              <Image
+                src={require("../assets/caitlin-o-headshot.jpeg")}
+                height="250"
+                roundedCircle
+              />
+              <h2>
+                <b>Caitlin O'Callaghan</b>
+              </h2>
               <p>Issues: {statsCO}</p>
               <p>Commits: {commitsCO}</p>
               <p>Unit Tests: 0</p>
             </div>
             <div className="column">
-              <Image src={require("../assets/cherry.jpeg")} height="250" roundedCircle/>
-              <h2><b>Cherry Sun</b></h2>
+              <Image
+                src={require("../assets/cherry.jpeg")}
+                height="250"
+                roundedCircle
+              />
+              <h2>
+                <b>Cherry Sun</b>
+              </h2>
               <p>Issues: {statsCS}</p>
               <p>Commits: {commitsCS}</p>
               <p>Unit Tests: 0</p>
             </div>
             <div className="column">
-              <Image src={require("../assets/LaurenMangibin-headshot.jpg")} height="250" roundedCircle/>
-              <h2><b>Lauren Mangibin</b></h2>
+              <Image
+                src={require("../assets/LaurenMangibin-headshot.jpg")}
+                height="250"
+                roundedCircle
+              />
+              <h2>
+                <b>Lauren Mangibin</b>
+              </h2>
               <p>Issues: {statsLM}</p>
               <p>Commits: {commitsLM}</p>
               <p>Unit Tests: 0</p>
             </div>
             <div className="column">
-              <Image src={require("../assets/samantha-headshot.jpg")} height="250" roundedCircle/>
-              <h2><b>Samantha Tuapen</b></h2>
+              <Image
+                src={require("../assets/samantha-headshot.jpg")}
+                height="250"
+                roundedCircle
+              />
+              <h2>
+                <b>Samantha Tuapen</b>
+              </h2>
               <p>Issues: {statsST}</p>
               <p>Commits: {commitsST}</p>
               <p>Unit Tests: 0</p>
             </div>
             <div className="column">
-              <Image src={require("../assets/uma-headshot.jpg")} height="250" roundedCircle/>
-              <h2><b>Uma Sethuraman</b></h2>
+              <Image
+                src={require("../assets/uma-headshot.jpg")}
+                height="250"
+                roundedCircle
+              />
+              <h2>
+                <b>Uma Sethuraman</b>
+              </h2>
               <p>Issues: {statsUS}</p>
               <p>Commits: {commitsUS}</p>
               <p>Unit Tests: 0</p>
             </div>
           </div>
         </div>
-        <div className = "h2_about"> 
+        <div className="h2_about">
           <br></br>
-          <h2><a href = "https://documenter.getpostman.com/view/12123261/TVRdAWse">Our Postman API</a></h2>
+          <h2>
+            <a href="https://documenter.getpostman.com/view/12123261/TVRdAWse">
+              Our Postman API
+            </a>
+          </h2>
           <br></br>
-          <h2><a href = "https://gitlab.com/caitlinlien/cs373-sustainability/">Our GitLab Repository</a></h2>
+          <h2>
+            <a href="https://gitlab.com/caitlinlien/cs373-sustainability/">
+              Our GitLab Repository
+            </a>
+          </h2>
           <br></br>
         </div>
       </body>
-      <CountryAPI/> 
+      <CountryAPI />
     </div>
   );
 }
@@ -194,9 +340,9 @@ export interface Counts {
 }
 
 export interface CommitsInfo {
-  name:      string;
-  email:     string;
-  commits:   number;
+  name: string;
+  email: string;
+  commits: number;
   additions: number;
   deletions: number;
 }
