8f7d76f37abb6dc3dcc0e028dce1cf68d20bca92
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-10-16 22:05:37 -0500

    years instance page still in progress

--- frontend/src/views/components/Year/YearInstance.tsx
+++ frontend/src/views/components/Year/YearInstance.tsx
@@ -2,61 +2,82 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "../../App.css";
 import Navbar from "../OurNavbar";
-import Image from "react-bootstrap/Image";
 import Table from "react-bootstrap/Table";
 import ImageBackground from "react";
 import View from "react";
-import "./YearInstance.css";
+import "./CountryInstance.css";
 import Carousel from "react-bootstrap/Carousel";
 import OurCarousel from "../OurCarousel";
+import OurMap from "../Map/OurMap";
 import Slide from "../../../Slide";
 import { useState, useEffect } from 'react';
+import axios from "axios";
+import Image from "react-bootstrap/Image";
 
-function YearInstance() {
-    const [countries, setCountries] = useState([]);
-    useEffect(() => {
-    fetch('/api/country').then(response =>
-        response.json().then(data => {
-            setCountries(data);
-        })
-    );
-    },[]);
-    console.log(countries);
 
-    return (
+const YearInstance = (name: any) => {
+
+  let [year, setYear] = React.useState<Year>();
+    
+  // gets data from API
+  const getData = () => {
+    axios.get("/api/years/name="+name.name)
+    .then((response)=>{
+        setYear(JSON.parse(JSON.stringify(response.data)) as Year);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+  };
+
+  getData();
+  
+  return (
     <div className="YearInstance">
-        <Navbar />
-        <header className="App-header">
-        <h1>
-            Year 2015
-        </h1>
+      <Navbar />
+      <header className="App-header">
+        <OurMap></OurMap>
+        {/* <Image src={flagLink} alt="Flag"/> */}
         <header className="Year-header">
-            <div className="image-text">
-            <h3> 2015 </h3>
-            </div>
+          <div className="image-text">
+            <h3> {year?.year_name} </h3>
+          </div>
         </header>
         <br />
-        <p>World Population: 7,349,472,099</p>
-        <p>Global Mean Surface Temperature Anomaly: 1.04</p>
-        <p>Carbon Dioxide Levels: 400.83 ppm</p>
-        <p>Methane Levels: 1834.23 ppm(?)</p>
-        <p>Nitrous Oxide Levels: 328.175 ppb</p>
-        <p>Ice Extent: 22245529.616666663 square kilometers</p>
-        <p>Absolute Sea Level Change since 1880: 8.943307703 inches</p>
-        <p>Top 10 Countries for Highest CO2 emissions: </p>
-        <p>Top 10 Cities for Highest Temperatures: </p>
-        <Image src={require("../../../assets/temperature-graph.jpg")}/>
-        <div>See more: </div>
-        <p>
-            <Link to="/climatechange/2013">2013</Link>
-        </p>
-        <p>
-            <Link to="/climatechange/2014">2014</Link>
-        </p>
-        
-        </header>
+        <Table bordered hover size="sm" variant="dark">
+          <tbody>
+            <tr>
+              <td>Income Level</td>
+              <td>{year?.yearName}</td>
+            </tr>
+            <tr>
+              <td>Region</td>
+              <td>{country?.country_region}</td>
+            </tr>
+            <tr>
+              <td>Capital City</td>
+              <td>{country?.country_capital_city}</td>
+            </tr>
+          </tbody>
+        </Table>
+      </header>
     </div>
-    );
+  );
+}
+
+export interface YearsObject {
+    years: Year[];
+}
+
+export interface Year {
+    co2:          number;
+    methane:      number;
+    nitrousOxide: number;
+    polarIce:     number;
+    seaLevel:     number;
+    tempAnomaly:  number;
+    yearID:       number;
+    yearName:     string;
 }
 
 export default YearInstance;
