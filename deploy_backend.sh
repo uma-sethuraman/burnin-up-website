8033d6da6eff6dd72bfc01d57a394c43fc0bfdd5
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-10-18 16:56:02 -0500

    adding frontend and backend deploy scripts

---
+++ deploy_backend.sh
@@ -0,0 +1,10 @@
+cd backend
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 170263725750.dkr.ecr.us-east-1.amazonaws.com
+docker build -t burning-backend .
+docker tag burninup-backend:latest 170263725750.dkr.ecr.us-east-1.amazonaws.com/burninup-backend:latest
+docker push 170263725750.dkr.ecr.us-east-1.amazonaws.com/burninup-backend:latest
+pip3 install awsebcli
+cd aws_deploy
+eb init
+eb deploy
+
