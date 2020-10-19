54f9c45ba63239e073c89a9f0b0d8651321be588
lauren.mangibin <lauren.mangibin@gmail.com>
2020-10-19 01:02:08 -0500

    Fully finished about page and landing

--- frontend/src/views/components/LandingPage/LandingPage.tsx
+++ frontend/src/views/components/LandingPage/LandingPage.tsx
@@ -13,6 +13,15 @@ function LandingPage() {
         </div>
       </header>
       <body className="Landing-body">
+        <br></br>
+        <div className="story">
+        Earth’s climate is changing faster than ever. The emission of pollutants in the air can result in 
+        serious changes to the climate. These pollutants can be extremely dangerous with harmful effects for
+         public health, ecosystems, and agricultural productivity. We want people to realize how serious the issue 
+         is by showing data that captures the impact of climate change around the world. We hope that our website shows 
+         a clear picture of how fast and how far the climate has changed around the world in the past decades and encourages 
+         people to take action to help the environment.  
+        </div>
         <div className="explore">
           <h3 style={{ color: "white" }}>Explore</h3>
           <div className="row">
