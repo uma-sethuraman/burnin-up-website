3b91418cde020674b72eef6f2d63f62c0928d69b
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-10-20 01:47:36 -0500

    added more selenium tests for about page

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -10,7 +10,6 @@ class tests(unittest.TestCase):
 
     def test_landing(self):
         self.driver.get("https://burninup.me")
-        print(self.driver.title)
         assert(self.driver.title == "Burnin' Up")
 
         self.driver.find_elements_by_xpath(
@@ -40,10 +39,21 @@ class tests(unittest.TestCase):
             "/html/body/div/div/div[1]/nav/div/a[4]")[0].click()
 
     def test_about(self):
-        pass
+        self.driver.get("https://burninup.me")
+        self.driver.find_elements_by_xpath("/html/body/div/div/div[1]/nav/div/a[1]")[0].click()
+        assert "about" in self.driver.current_url
+        title = self.driver.find_elements_by_xpath("/html/body/div/div/body/div[1]/h3")[0].text
+        assert title == "About Us"
+        self.driver.find_elements_by_xpath("/html/body/div/div/body/div[5]/h2[1]/a/img")[0].click()
+        assert "documenter.getpostman.com/view/12123261/TVRdAWse" in self.driver.current_url
+        self.driver.get("https://burninup.me/about")
+        self.driver.find_elements_by_xpath("/html/body/div/div/body/div[5]/h2[2]/a/img")[0].click()
+        assert "gitlab.com/caitlinlien/cs373-sustainability/" in self.driver.current_url
 
-    # def test_cities(self):
-    #     pass
+    def test_cities(self):
+        self.driver.get("https://burninup.me")
+        self.driver.find_elements_by_xpath("/html/body/div/div/div[1]/nav/div/a[2]")[0].click()
+        assert "cities" in self.driver.current_url
 
     # def test_countries(self):
     #     pass
