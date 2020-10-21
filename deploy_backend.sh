085c9d9c3c79e9cc6cacf00a483dc35f62b60177
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-10-20 20:33:22 -0500

    pushing script again

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,4 +1,7 @@
-cd backend
+cd frontend
+sudo npm install
+sudo npm run build
+cd ../backend
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 170263725750.dkr.ecr.us-east-1.amazonaws.com
 docker build -t burning-backend .
 docker tag burninup-backend:latest 170263725750.dkr.ecr.us-east-1.amazonaws.com/burninup-backend:latest
