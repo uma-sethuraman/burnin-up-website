251b6b0b3eca16a6601dd5faddfe9292df5ccb1c
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-10-20 21:09:22 -0500

    fixing script

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -3,11 +3,11 @@ sudo npm install
 sudo npm run build
 cd ../backend
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 170263725750.dkr.ecr.us-east-1.amazonaws.com
-docker build -t burning-backend .
+docker build -t burninup-backend .
 docker tag burninup-backend:latest 170263725750.dkr.ecr.us-east-1.amazonaws.com/burninup-backend:latest
 docker push 170263725750.dkr.ecr.us-east-1.amazonaws.com/burninup-backend:latest
 pip3 install awsebcli
 cd aws_deploy
 #eb init
-eb deploy --staged
+eb deploy
 
