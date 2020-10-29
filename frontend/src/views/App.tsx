3cfa095bddc470193b722203a1758cfb496d2783
lauren.mangibin <lauren.mangibin@gmail.com>
2020-10-28 22:42:14 -0500

    Added instant search part 2

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -3,38 +3,25 @@ import Button from 'react-bootstrap/Button';
 import Navbar from './components/OurNavbar';
 import Image from "react-bootstrap/Image"; 
 import LandingPage from './components/LandingPage/LandingPage';
-// import SearchHelper from './helpers';
-// global instantsearch;
-
 import algoliasearch from 'algoliasearch/lite';
-import instantsearch from 'instantsearch.js';
-import { searchBox, hits } from 'instantsearch.js/es/widgets';
+import { InstantSearch } from 'react-instantsearch-dom';
 
+const searchClient = algoliasearch(
+  '55BA5YQNJQ',
+  '0fb7c07cd58d91990a6ca879e44885e5'
+);
 
 function App() {
-  const searchClient = algoliasearch('YourApplicationID', '0fb7c07cd58d91990a6ca879e44885e5');
-
-  const search = instantsearch({
-    indexName: 'demo_ecommerce',
-    searchClient,
-  });
-
-  search.addWidgets([
-    searchBox({
-      container: "#searchbox"
-    }),
-
-    hits({
-      container: "#hits"
-    })
-  ]);
-
-  search.start();
   return (
     <div className="App">
       <Navbar />
-      {/* <SearchHelper/> */}
-      {/* <LandingPage /> */}
+      <InstantSearch
+        indexName="prod_BURNINUP"
+        searchClient={searchClient}
+      >
+        {/* Widgets */}
+      </InstantSearch>
+      <LandingPage />
     </div>
   );
 }
