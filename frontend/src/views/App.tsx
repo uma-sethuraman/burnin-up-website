e6d334aa814162f1fda4c58676f06797a14c2477
lauren.mangibin <lauren.mangibin@gmail.com>
2020-10-28 22:14:29 -0500

    adding instantsearch-- not complete

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -3,13 +3,38 @@ import Button from 'react-bootstrap/Button';
 import Navbar from './components/OurNavbar';
 import Image from "react-bootstrap/Image"; 
 import LandingPage from './components/LandingPage/LandingPage';
+// import SearchHelper from './helpers';
+// global instantsearch;
+
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+
 
 function App() {
-  
+  const searchClient = algoliasearch('YourApplicationID', '0fb7c07cd58d91990a6ca879e44885e5');
+
+  const search = instantsearch({
+    indexName: 'demo_ecommerce',
+    searchClient,
+  });
+
+  search.addWidgets([
+    searchBox({
+      container: "#searchbox"
+    }),
+
+    hits({
+      container: "#hits"
+    })
+  ]);
+
+  search.start();
   return (
     <div className="App">
       <Navbar />
-      <LandingPage />
+      {/* <SearchHelper/> */}
+      {/* <LandingPage /> */}
     </div>
   );
 }
