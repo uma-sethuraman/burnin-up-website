0abc2ca7d0067dabb0ff4443eb4258dab5d7c093
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-10-28 23:37:02 -0500

    got main page search to work for cities but need to figure out how to link pages from search

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Navbar from './components/OurNavbar';
-import Image from "react-bootstrap/Image"; 
+import Image from "react-bootstrap/Image";
 import LandingPage from './components/LandingPage/LandingPage';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch } from 'react-instantsearch-dom';
+import { SearchBox } from 'react-instantsearch-dom';
+import { Hits } from 'react-instantsearch-dom';
+import { Highlight } from 'react-instantsearch-dom';
 
 const searchClient = algoliasearch(
   '55BA5YQNJQ',
   '0fb7c07cd58d91990a6ca879e44885e5'
 );
+
+// const Hit = (hit:any) =>
+//   <div className="hit">
+//     <div className = "hitName">
+//       <Highlight attribute = "city_name" hit = {hit.city_name}/>
+//     </div>
+//   </div>
+
+// const Content = () =>
+//   <div className = "content">
+//     {/* {console.log(Hit)} */}
+//     <Hits/>
+//     {/* <Hits hitComponent = {Hit}/> */}
+//   </div>
 
 function App() {
   return (
@@ -19,7 +36,8 @@ function App() {
         indexName="prod_BURNINUP"
         searchClient={searchClient}
       >
-        {/* Widgets */}
+        <SearchBox />
+        <Hits />
       </InstantSearch>
       <LandingPage />
     </div>
