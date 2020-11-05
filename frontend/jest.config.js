11bd22290f63eacd38240dcc2664466f077af0a6
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-11-04 22:02:11 -0600

    fixing more files to fulfill 80 char limit

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,12 +5,16 @@ module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\.(ts|tsx)?$': 'ts-jest',
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf)$": "<rootDir>/assetsTransformer.js",
+    "\\.(webp|svg|ttf|woff|woff2|mp4|webm)$": "<rootDir>/assetsTransformer.js",
+    "\\.(wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js"
   },
   testRegex: '(/tests/.*|(\.|/)(test|spec))\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(webp|svg|ttf|woff|woff2|mp4)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
     "\\.(css|less)$": "identity-obj-proxy"
   },
   globals: {
