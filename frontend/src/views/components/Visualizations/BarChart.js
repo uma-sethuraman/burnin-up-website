75d41ba9292940b18b76654e6cbaec3fa8f0e591
cherrysun9 <cherrysun9@utexas.edu>
2020-11-16 22:29:11 -0600

    add function

--- frontend/src/views/components/Visualizations/BarChart.js
+++ frontend/src/views/components/Visualizations/BarChart.js
@@ -17,6 +17,9 @@ class BarChart extends Component {
     }
 
     createBarChart(){
+		var myColor = d3.scaleLinear().domain([1,10])
+		.range(["white", "blue"])
+		
         const margin = { top: 50, right: 20, bottom: 100, left: 60};
         const height = 600 - margin.top - margin.bottom;
         const width = 1200 - margin.left - margin.right;
@@ -59,8 +62,8 @@ class BarChart extends Component {
             .attr('y', d => y(d[this.props.yAttr]))
             .attr('width', x.bandwidth())
             .attr('height', d => height - y(d[this.props.yAttr]))
-            .attr('fill', '#2bc4ad');
-
+            .attr("fill", function(d){return myColor(d) })
+			
     }
 
     render(){
