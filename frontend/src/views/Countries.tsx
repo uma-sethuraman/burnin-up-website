796fc1ed300427d05e5ce989baba3b6ac870dcfa
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-11-17 01:39:45 -0600

    updated countries general page to final style, added CountriesTable component

--- frontend/src/views/Countries.tsx
+++ frontend/src/views/Countries.tsx
@@ -1,19 +1,28 @@
 import React from "react";
-import Image from "react-bootstrap/Image";
 import Navbar from "./components/OurNavbar";
 import { CountriesObject, Country } from "./components/Country/CountryInstance";
 import { useState, useEffect } from 'react';
 import useAxios from 'axios-hooks';
-import MUIDataTable from "mui-datatables";
 import "./Countries.css";
-import {MUIDataTableColumnDef} from "mui-datatables";
 import Loading from "./components/Loading";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
+import WebFont from "webfontloader";
+import CountriesTable from "./components/CountriesTable/CountriesTable";
 
 /* general countries model page (route: "/countries") */
 const Countries = () => {
   
   /* array of all countries retrieved from api */
   const [countries, setCountries] = useState<Country[]>([]);
+
+  /* text passed into search bar */
+  const [search_text, setSearchText] = useState<string>("");
+
+  /* reference for search bar input */
+  const text_input: any = React.useRef();
 
   /* loads api data into data */
   const [{ data, loading, error }] = useAxios('/api/countries')
@@ -31,194 +40,34 @@ const Countries = () => {
     }
   }, [data]);
 
-  /* all columns of the countries table, with sorting
-  and filtering options set */
-  const columns = [
-    {
-      name: "country_id",
-      label: "Country ID",
-      options: {
-        filter: false,
-        sort: false,
-        display: "excluded",
-      }
-    },
-    {
-      name: "country_name",
-      label: "Country",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['A-I', 'J-R', 'S-Z'],
-          logic(country_name: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('A-I') >= 0 &&
-                country_name.charCodeAt(0) >= ('A'.charCodeAt(0)) &&
-                country_name.charCodeAt(0) <= ('I'.charCodeAt(0))) ||
+  const styles = {
+    searchButton: {
+      backgroundColor: "white", 
+      borderColor: "black", 
+      color: "black",
+    } as React.CSSProperties,
+  } 
 
-              (filterVal.indexOf('J-R') >= 0 &&
-                country_name.charCodeAt(0) >= ('J'.charCodeAt(0)) &&
-                country_name.charCodeAt(0) <= ('R'.charCodeAt(0))) ||
+  /* saves search bar input on "search" click
+  or when clicking enter. if clear = true,
+  that means user clicked "clear" button so reset
+  search text */
+  function searchOnClick(clear: boolean) {
+    if (clear) {
+      setSearchText("");
+      text_input.current.value = "";
+    }
+    else
+      setSearchText(text_input.current.value);
+  }
 
-              (filterVal.indexOf('S-Z') >= 0 &&
-                country_name.charCodeAt(0) >= ('S'.charCodeAt(0)) &&
-                country_name.charCodeAt(0) <= ('Z'.charCodeAt(0)));
-            return !show;
-          },
-        },
-      },
+  WebFont.load({
+    google: {
+      families: [
+        "Raleway",
+      ],
     },
-    {
-      name: "income_level",
-      label: "Income Level",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['High Income', 'Upper Middle Income', 
-          'Lower Middle Income', 'Low Income'],
-          logic(income_level: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('High Income') >= 0 &&
-                (income_level === "High income" || 
-                income_level === "High Income")) ||
-
-              (filterVal.indexOf('Upper Middle Income') >= 0 &&
-                (income_level === "Upper middle income" || 
-                income_level === "Upper Middle Income")) ||
-
-              (filterVal.indexOf('Lower Middle Income') >= 0 &&
-                (income_level === "Lower middle income")) ||
-
-              (filterVal.indexOf('Low Income') >= 0 &&
-                (income_level === "Low income"));
-            return !show;
-          },
-        },
-      },
-    },
-    {
-      name: "country_region",
-      label: "Country Region",
-      options: {
-        filter: true,
-        sort: true,
-      },
-    },
-    {
-      name: "country_capital_city",
-      label: "Capital City",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['A-I', 'J-R', 'S-Z'],
-          logic(city_name: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('A-I') >= 0 &&
-                city_name.charCodeAt(0) >= ('A'.charCodeAt(0)) &&
-                city_name.charCodeAt(0) <= ('I'.charCodeAt(0))) ||
-
-              (filterVal.indexOf('J-R') >= 0 &&
-                city_name.charCodeAt(0) >= ('J'.charCodeAt(0)) &&
-                city_name.charCodeAt(0) <= ('R'.charCodeAt(0))) ||
-
-              (filterVal.indexOf('S-Z') >= 0 &&
-                city_name.charCodeAt(0) >= ('S'.charCodeAt(0)) &&
-                city_name.charCodeAt(0) <= ('Z'.charCodeAt(0)));
-            return !show;
-          },
-        },
-      },
-    },
-    {
-      name: "country_population",
-      label: "Population",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['Small Population', 'Medium Population', 'Large Population'],
-          logic(country_population: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('Small Population') >= 0 && 
-              country_population < 1000000) ||
-              (filterVal.indexOf('Medium Population') >= 0 && 
-              country_population >= 1000000 && 
-              country_population < 100000000) ||
-              (filterVal.indexOf('Large Population') >= 0 && 
-              country_population >= 100000000);
-
-            return !show;
-          },
-        },
-      },
-    },
-    {
-      name: "lat",
-      label: "Latitude",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['Northern Hemisphere', 'Southern Hemisphere'],
-          logic(lat: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('Northern Hemisphere') >=0 && lat >= 0) ||
-              (filterVal.indexOf('Southern Hemisphere') >= 0 && lat < 0);
-            return !show;
-          },
-        },
-      },
-    },
-    {
-      name: "long",
-      label: "Longitude",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['Western Hemisphere', 'Eastern Hemisphere'],
-          logic(lon: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('Western Hemisphere') >= 0 && lon < 0) ||
-              (filterVal.indexOf('Eastern Hemisphere') >= 0 && lon >= 0);
-            return !show;
-          },
-        },
-      },
-    },
-    {
-      name: "recent_emissions",
-      label: "Most Recent CO2 Emissions (ppm)",
-      options: {
-        filter: true,
-        sort: true,
-        filterOptions: {
-          names: ['Low CO2', 'Medium CO2', 'High CO2'],
-          logic(co2: any, filterVal: any) {
-            const show =
-              (filterVal.indexOf('Low CO2 Emissions') >= 0 && co2 < 50) ||
-              (filterVal.indexOf('Medium CO2 Emissions') >= 0 && 
-              co2 >= 50 && 
-              co2 < 100) ||
-              (filterVal.indexOf('High CO2 Emissions') >= 0 && co2 >= 100);
-            return !show;
-          },
-        },
-      },
-    },
-  ];
-
-  /* options for the countries table, initializing OnRowClick
-  to redirect to that row's country page during a click */
-  const options = {
-    filterType: 'checkbox' as any,
-    onRowClick: (rowData: any) => {
-      window.location.assign('/countries/id=' + rowData[0]);
-    },
-  };
+  });
 
   return (
     <div className="Countries">
@@ -231,27 +80,42 @@ const Countries = () => {
           <div className = "Countries-h1">
             <h1 className="Countries-h1">Countries</h1>
           </div>
-          <br />
-          
-          <div className="side-by-side">
 
-          {/* displaying instructions on how to sort and filter */}
-          <Image src={require("../assets/filter_icon.png")} width="50px" fluid/>
-            <p>&nbsp;&nbsp;Click this filter icon in the table to filter by any
-              column.</p>
-            </div>
-          <p>Click on a column name to sort by that column.</p>
+          {/* countries search bar */}
+          <Form
+              inline
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <FormControl
+                className="mr-sm-2"
+                type="text"
+                placeholder="Search Countries"
+                ref={text_input}
+                onKeyPress={(event: any) => {
+                  if (event.key === "Enter") {
+                    searchOnClick(false); /* save search input */
+                  }
+                }}
+              />
+
+              {/* search and clear buttons */}
+              <Button style={styles.searchButton} variant="info" onClick={() => searchOnClick(false)}>
+                <AiOutlineSearch />
+              </Button>
+              
+              <Button style={styles.searchButton} variant="info" onClick={() => searchOnClick(true)}>
+                <AiOutlineClose />
+              </Button>
+            </Form>
+            <br />
           </header>
 
           {/* displaying table with all country instances */}
-          <div style={{ display: 'table', tableLayout: 'fixed', width: '100%' }}>
-            <MUIDataTable
-              title={<div className = "table">Countries Data</div>}
-              data={countries}
-              columns={columns as MUIDataTableColumnDef[]}
-              options={options}
-            />
-          </div>
+          <CountriesTable
+            countriesArray={countries}
+            searchQuery={search_text} />
         </div>}
     </div>
   );
---
+++ frontend/src/views/components/CountriesTable/CountriesTable.tsx
@@ -0,0 +1,278 @@
+import React from "react";
+import "./CountriesTable.css";
+import MUIDataTable from "mui-datatables";
+import { MUIDataTableColumnDef } from "mui-datatables";
+import Highlighter from "react-highlight-words";
+import WebFont from "webfontloader";
+
+/* displays table of all countries in
+countries general page */
+function CountriesTable(props:any) {
+
+    /* custom render for countries table elements to allow for
+    highlighting of search terms */
+    const countryCustomBodyRender = (value: any, tableMeta: any, updateValue: any) => 
+        <div>
+            <Highlighter
+                highlightClassName="highlight-class"
+                searchWords={[props.searchQuery]}
+                textToHighlight={value+""}>
+            </Highlighter>
+        </div>
+
+    /* all columns of the countries table, with sorting
+    and filtering options set */
+    const columns = [
+        {
+        name: "country_id",
+        label: "Country ID",
+        options: {
+            filter: false,
+            sort: false,
+            display: "excluded",
+        }
+        },
+        {
+        name: "country_name",
+        label: "Country",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['A-I', 'J-R', 'S-Z'],
+            logic(country_name: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('A-I') >= 0 &&
+                    country_name.charCodeAt(0) >= ('A'.charCodeAt(0)) &&
+                    country_name.charCodeAt(0) <= ('I'.charCodeAt(0))) ||
+
+                (filterVal.indexOf('J-R') >= 0 &&
+                    country_name.charCodeAt(0) >= ('J'.charCodeAt(0)) &&
+                    country_name.charCodeAt(0) <= ('R'.charCodeAt(0))) ||
+
+                (filterVal.indexOf('S-Z') >= 0 &&
+                    country_name.charCodeAt(0) >= ('S'.charCodeAt(0)) &&
+                    country_name.charCodeAt(0) <= ('Z'.charCodeAt(0)));
+                return !show;
+            },
+            },
+            /* enable highlighting */
+            customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "income_level",
+        label: "Income Level",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['High Income', 'Upper Middle Income', 
+            'Lower Middle Income', 'Low Income'],
+            logic(income_level: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('High Income') >= 0 &&
+                    (income_level === "High income" || 
+                    income_level === "High Income")) ||
+
+                (filterVal.indexOf('Upper Middle Income') >= 0 &&
+                    (income_level === "Upper middle income" || 
+                    income_level === "Upper Middle Income")) ||
+
+                (filterVal.indexOf('Lower Middle Income') >= 0 &&
+                    (income_level === "Lower middle income")) ||
+
+                (filterVal.indexOf('Low Income') >= 0 &&
+                    (income_level === "Low income"));
+                return !show;
+            },
+            },
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "country_region",
+        label: "Country Region",
+        options: {
+            filter: true,
+            sort: true,
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "country_capital_city",
+        label: "Capital City",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['A-I', 'J-R', 'S-Z'],
+            logic(city_name: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('A-I') >= 0 &&
+                    city_name.charCodeAt(0) >= ('A'.charCodeAt(0)) &&
+                    city_name.charCodeAt(0) <= ('I'.charCodeAt(0))) ||
+
+                (filterVal.indexOf('J-R') >= 0 &&
+                    city_name.charCodeAt(0) >= ('J'.charCodeAt(0)) &&
+                    city_name.charCodeAt(0) <= ('R'.charCodeAt(0))) ||
+
+                (filterVal.indexOf('S-Z') >= 0 &&
+                    city_name.charCodeAt(0) >= ('S'.charCodeAt(0)) &&
+                    city_name.charCodeAt(0) <= ('Z'.charCodeAt(0)));
+                return !show;
+            },
+            },
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "country_population",
+        label: "Population",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['Small Population', 'Medium Population', 'Large Population'],
+            logic(country_population: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('Small Population') >= 0 && 
+                country_population < 1000000) ||
+                (filterVal.indexOf('Medium Population') >= 0 && 
+                country_population >= 1000000 && 
+                country_population < 100000000) ||
+                (filterVal.indexOf('Large Population') >= 0 && 
+                country_population >= 100000000);
+
+                return !show;
+            },
+            },
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "lat",
+        label: "Latitude",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['Northern Hemisphere', 'Southern Hemisphere'],
+            logic(lat: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('Northern Hemisphere') >=0 && lat >= 0) ||
+                (filterVal.indexOf('Southern Hemisphere') >= 0 && lat < 0);
+                return !show;
+            },
+            },
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "long",
+        label: "Longitude",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['Western Hemisphere', 'Eastern Hemisphere'],
+            logic(lon: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('Western Hemisphere') >= 0 && lon < 0) ||
+                (filterVal.indexOf('Eastern Hemisphere') >= 0 && lon >= 0);
+                return !show;
+            },
+            },
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+        {
+        name: "recent_emissions",
+        label: "Most Recent CO2 Emissions (ppm)",
+        options: {
+            filter: true,
+            sort: true,
+            filterOptions: {
+            names: ['Low CO2', 'Medium CO2', 'High CO2'],
+            logic(co2: any, filterVal: any) {
+                const show =
+                (filterVal.indexOf('Low CO2 Emissions') >= 0 && co2 < 50) ||
+                (filterVal.indexOf('Medium CO2 Emissions') >= 0 && 
+                co2 >= 50 && 
+                co2 < 100) ||
+                (filterVal.indexOf('High CO2 Emissions') >= 0 && co2 >= 100);
+                return !show;
+            },
+            },
+             /* enable highlighting */
+             customBodyRender: (value: any, tableMeta: any, updateValue: any) => 
+                countryCustomBodyRender(value, tableMeta, updateValue)
+        },
+        },
+    ];
+
+    /* options for the countries table, initializing OnRowClick
+    to redirect to that row's country page during a click */
+    const options = {
+        filterType: 'checkbox' as any,
+        onRowClick: (rowData: any) => {
+            window.location.assign('/countries/id=' + rowData[0]);
+        },
+        searchText: props.searchQuery, /* what is searched for */
+        search: false,
+        download: false,
+        print: false,
+        selectableRowsHideCheckboxes: true,
+        selectableRowsHeader: false,
+        viewColumns: false,
+    };
+
+    const styles = {
+        table: {
+          display: "table", 
+          tableLayout: "fixed", 
+          width: "100%",
+        } as React.CSSProperties
+    } 
+    WebFont.load({
+        google: {
+          families: [
+            "Raleway",
+          ],
+        },
+    });
+
+    return (
+        <div className="CountriesTable">
+            {/* displaying the table of all countries, 
+            with searching and pagination */}
+            <div style={styles.table}>
+                <MUIDataTable
+                    title={
+                      <div className="table-title">
+                        <p>Click the filter icon in the table toolbar to filter.</p>
+                        <p>Click on a column name to sort by that column.</p>
+                      </div>}
+                    data={props.countriesArray}
+                    columns={columns as MUIDataTableColumnDef[]}
+                    options={options}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default CountriesTable;
