77d627d9ea26c07c7915f8290b348a0bf001e7b1
samantha3pen <samtuapen@utexas.edu>
2020-11-20 17:43:02 -0600

    cleaned up visualizations page

--- frontend/src/views/Visualizations.tsx
+++ frontend/src/views/Visualizations.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Navbar from "./components/OurNavbar";
 import "./Visualizations.css";
-import country from "../vizdata/countries.json"
-
-import energy_sources from "../vizdata/energy_sources.json"
-
 import PieChart from "./components/Visualizations/PieChart";
-import carbonemissions from "../vizdata/CarbonEmissions.json";
+import manufacturers from "../vizdata/Manufacturer.json";
 import BarChart from "./components/Visualizations/BarChart";
+import energy_sources from "../vizdata/energy_sources.json";
+import country from "../vizdata/countries.json";
 import BubbleChart from '@weknow/react-bubble-chart-d3';
 import citytemps from "../vizdata/CityRecentTempsRanges.json";
 import fuelingstations from "../vizdata/FuelingStationsPerState.json";
-import manufacturers from "../vizdata/Manufacturer.json";
-
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import carbonemissions from "../vizdata/CarbonEmissions.json";
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -21,10 +19,7 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
-import {
-	LineChart, Line, XAxis, YAxis
-} from 'recharts';
-
+/* allows for tabs in the appbar  */
 function TabPanel(props) {
 	const { children, value, index, ...other } = props;
 
@@ -45,12 +40,14 @@ function TabPanel(props) {
 	);
 }
 
+/* props to be used in TabPanel */
 TabPanel.propTypes = {
 	children: PropTypes.node,
 	index: PropTypes.any.isRequired,
 	value: PropTypes.any.isRequired,
 };
 
+/* distinguishes tabs on appbar */
 function a11yProps(index) {
 	return {
 	  id: `simple-tab-${index}`,
@@ -58,18 +55,11 @@ function a11yProps(index) {
 	};
 }
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-	  flexGrow: 1,
-	  backgroundColor: theme.palette.background.paper,
-	},
-}));
-
-/* page for Helpful Links */
+/* page for Visualizations */
 function Visuals() {
-	const classes = useStyles();
 	const [value, setValue] = React.useState(0);
 
+	/* event listener for navigating tabs */
 	const handleChange = (event, newValue) => {
 		setValue(newValue);
 	};	
@@ -77,23 +67,36 @@ function Visuals() {
 	return (
 		<div className="Visuals">
 			<Navbar />
-		
+
 			<div className="Visuals-body">
 				<h3>Visualizations</h3>
 				<br />
-
+				{/* appbar for navigating between visualizations */}
 				<AppBar position="static" style={{ background : '#000000'}} >
-					<Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered >
+					<Tabs 
+						value={value} 
+						onChange={handleChange} 
+						aria-label="simple tabs example" 
+						centered 
+					>
 						<Tab label="Countries" {...a11yProps(0)} />
 						<Tab label="Cities" {...a11yProps(1)} />
 						<Tab label="Annual Climate Change" {...a11yProps(2)} />
 						<Tab label="Provider's Visualizations" {...a11yProps(3)} />
 					</Tabs>
 				</AppBar>
+				{/* tab for countries visualization */}
 				<TabPanel value={value} index={0}>
-					<h2>Top 50 Countries with Highest Carbon Emissions</h2>
-					{ <BarChart data={country} xAttr="country" yAttr="emissions" xLabel="Countries" yLabel="Emissions"/>} 
+					<h2>Top 50 Countries with The Highest Carbon Emissions</h2>
+					<BarChart 
+						data={country} 
+						xAttr="country" 
+						yAttr="emissions" 
+						xLabel="Countries" 
+						yLabel="Carbon Emissions (ppm)"
+					/>
 				</TabPanel>
+				{/* tab for cities visualization */}
 				<TabPanel value={value} index={1}>
 					<h2>Recent City Temperatures Worldwide Grouped by Range:</h2>
 					<BubbleChart
@@ -104,9 +107,9 @@ function Visuals() {
 							}}
 							width={1400}
 							height={1000}
-							padding={15} // optional value, number that set the padding between bubbles
-							showLegend={true} // optional value, pass false to disable the legend.
-							legendPercentage={20} // number that represent the % of with that legend going to use.
+							padding={15} 
+							showLegend={true} 
+							legendPercentage={20} 
 							legendFont={{
 								family: 'Arial',
 								size: 16,
@@ -128,27 +131,52 @@ function Visuals() {
 						data={citytemps}
 					/>
 				</TabPanel>
+				{/* tab for annual climate change visualization */}
 				<TabPanel value={value} index={2}>
 					<div className="carbonemissions-vis" style={{color: "white"}}>
 						<h2>Mean Global Carbon Emissions from 1880-2020</h2>
-						{/* <ConnectedScatterplot data={carbonemissions} xAttr="Year" yAttr="CarbonEmissions" xMin={1880} xMax={2020} yMin={200} yMax={500} xLabel="Year" yLabel="Carbon Emissions (ppm)" /> */}
 						<LineChart
 							width={1000}
 							height={500}
 							data={carbonemissions}
 							margin={{top: 20, right: 20, left: 300, bottom: 20,}}
 						>
-							<XAxis dataKey="Year" stroke="white" label={{value: 'Year', position: "insideBottomRight", dx: -250, dy: 25, fill:'white', fontSize: 20}} tick={{fontSize: 12}} domain={['dataMin', 'dataMax']} type='number' tickCount={16}/>
-							<YAxis dataKey="CarbonEmissions" stroke="white" label={{value: 'Carbon Emissions (ppm)', position: "insideLeft", angle: -90, dx: -10, dy: 90, fill:'white', fontSize: 20}} tick={{fontSize: 12}} domain={[270, 'auto']} tickCount={35} interval={1} />
-							<Line type="monotone" dataKey="CarbonEmissions" stroke="white" dot={false} strokeWidth="3" />
+							<XAxis 
+								dataKey="Year" 
+								stroke="white" 
+								label={{value: 'Year', position: "insideBottomRight", 
+									dx: -250, dy: 25, fill:'white', fontSize: 20}} 
+								tick={{fontSize: 12}} 
+								domain={['dataMin', 'dataMax']} 
+								type='number' 
+								tickCount={16}
+							/>
+							<YAxis 
+								dataKey="CarbonEmissions" 
+								stroke="white" 
+								label={{value: 'Carbon Emissions (ppm)', 
+									position: "insideLeft", angle: -90, dx: -10, 
+									dy: 90, fill:'white', fontSize: 20}} 
+								tick={{fontSize: 12}} 
+								domain={[270, 'auto']} 
+								tickCount={35} 
+								interval={1} 
+							/>
+							<Line 
+								type="monotone" 
+								dataKey="CarbonEmissions" 
+								stroke="white" dot={false} 
+								strokeWidth="3" 
+							/>
 						</LineChart>
 					</div>
 				</TabPanel>
+				{/* tab for provider's visualizations */}
 				<TabPanel value={value} index={3}>
 					<h2>Number of Fueling Stations per State in the US:</h2>
 					<BubbleChart
 						graph={{
-							zoom: 0.90,
+							zoom: 0.85,
 							offsetX: 0.00,
 							offsetY: -0.01
 						}}
@@ -156,7 +184,7 @@ function Visuals() {
 						height={1000}
 						padding={15} 
 						showLegend={true} 
-						legendPercentage={20} 
+						legendPercentage={15} 
 						legendFont={{
 							family: 'Arial',
 							size: 10,
@@ -180,13 +208,18 @@ function Visuals() {
 					<br />
 					<br />
 					<h2>Number of Eco-Friendly Cars Per Manufacturer:</h2>
-					{<PieChart data={manufacturers} />}
+					<PieChart data={manufacturers} />
 					<br />
 					<br />
-					<h2>Energy Resource vs Total Power</h2>
-					{<BarChart data={energy_sources} xAttr="energe" yAttr="power" xLabel="Energy" yLabel="Power"/>}
+					<h2>Total Power per Energy Source</h2>
+					<BarChart 
+						data={energy_sources} 
+						xAttr="energe" 
+						yAttr="power" 
+						xLabel="Energy Source" 
+						yLabel="Total Power (MWh)"
+					/>
 				</TabPanel>
-			
 			</div>
 		</div>
 	);
